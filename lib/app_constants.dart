22c0f43fdb3c118275f4ded379983c495ba76495
Esraa Ashraf <ea624439@gmail.com>
2024-04-14 03:12:36 +0200

    Create structure of files and add assets.

---
+++ lib/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants{
+  static const String splash = 'assets/images/splash.png';
+  static const String browse = 'assets/images/browse.png';
+  static const String listWatched = 'assets/images/list_watched.png';
+  static const String searchLogo = 'assets/images/search_logo.png';
+  static const String searchIcon = 'assets/images/search_icon.png';
+  static const String home = 'assets/images/home.png';
+}
